685571595120f8493dc9887a4e23b261f3c8e82e
Sebastian Langguth <sebastianlangguth@MacBook-Pro.local>
2018-09-23 00:31:38 -0500

    add props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact
+          name="Karen Smith"
+          email="karen@gmail.com"
+          phone="777-777-7777"
+        />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = props => {
+  const { branding } = props;
+  return (
+    <div>
+      <h1>{props.branding}</h1>
+    </div>
+  );
+};
+
+export default Header;
